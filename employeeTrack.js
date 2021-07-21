49c72f59e8d8224127f6e6e8438f7d13a17efe9b
Junny <tajo0215@gmail.com>
2021-07-20 22:37:56 -0700

    make connection work

---
+++ employeeTrack.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+connection.connect(err => {
+  if (err) throw err;
+  console.log("Connection Begins...");
+})
